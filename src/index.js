ed94349358ceeb887107fda14a033a6b6a890f67
Ashley Neaves <ashley.neaves@stfc.ac.uk>
2022-10-14 09:41:24 +0100

    attempt to fix export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+// import OdinNav from './OdinNav'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
 
-export { default as OdinNav } from "./OdinNav";
+export { default } from './OdinNav';
